fb130b0a60fe150c1384662ff43dd80864c0c752
Dipak 2056 <paudelsantosh508@gmail.com>
2022-05-10 13:56:38 +1000

    login functionality successfully implemented

--- frontend-expenses-tracker/src/components/layout/Login.jsx
+++ frontend-expenses-tracker/src/components/layout/Login.jsx
@@ -1,23 +1,54 @@
-import React from "react";
-import { Form, Row, Button } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Form, Row, Button, Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { postLogin } from "../../helpers/axiosHelper";
 
 export const Login = () => {
+  const emailref = useRef("");
+  const passwordref = useRef("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleOnSubmit = async () => {
+    const email = emailref.current.value;
+    const password = passwordref.current.value;
+
+    if (!email || !password) {
+      return alert("please enter email and password ");
+    }
+    setLoading(true);
+    const { data } = await postLogin({ email, password });
+    setLoading(false);
+    console.log(data);
+
+    if (data.status === "success") {
+      //if login success, store user dataa in session storage
+      const { name, email, _id } = data.user;
+      sessionStorage.setItem("user", JSON.stringify(data.user));
+    }
+
+    //else show the error message
+    setError(data.message);
+  };
   return (
     <Row className="login-comp mt-5">
       <Form>
         <h3>Welcome back</h3>
         <Form.Group className="mb-3" controlId="formGroupEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control ref={emailref} type="email" placeholder="Enter email" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formGroupPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            ref={passwordref}
+            type="password"
+            placeholder="Password"
+          />
         </Form.Group>
 
-        <Button variant="primary" type="submit">
+        <Button variant="primary" onClick={handleOnSubmit}>
           Login
         </Button>
         <div className="text-end">
--- frontend-expenses-tracker/src/helpers/axiosHelper.js
+++ frontend-expenses-tracker/src/helpers/axiosHelper.js
@@ -1,10 +1,24 @@
 import axios from "axios";
 const rootURL = "http://localhost:8000/api/v1";
 const userAPI = rootURL + "/users";
+const loginApi = rootURL + "/users/login";
 
 export const postRegister = (formDt) => {
   try {
     return axios.post(userAPI, formDt);
+  } catch (error) {
+    const data = {
+      status: "error",
+      message: error.message,
+    };
+    return {
+      data,
+    };
+  }
+};
+export const postLogin = (formDt) => {
+  try {
+    return axios.post(loginApi, formDt);
   } catch (error) {
     const data = {
       status: "error",
