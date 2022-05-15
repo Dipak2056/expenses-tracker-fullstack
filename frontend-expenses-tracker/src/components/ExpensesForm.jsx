7d973db7621d936d5a6ddde3c07286826f67adfb
Dipak 2056 <paudelsantosh508@gmail.com>
2022-05-16 09:36:52 +1000

    got data from form in console

--- frontend-expenses-tracker/src/components/ExpensesForm.jsx
+++ frontend-expenses-tracker/src/components/ExpensesForm.jsx
@@ -1,38 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col, Form, FormControl, Button } from "react-bootstrap";
 
+const initialState = {
+  name: "",
+  amount: " ",
+  date: "",
+};
 export const ExpensesForm = () => {
+  const [frmData, setFrmData] = useState(initialState);
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(frmData);
+  };
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setFrmData({
+      ...frmData,
+      [name]: value,
+    });
+  };
   return (
-    <Row className="g-3 bg-dark p-4">
-      <Col md="4">
-        <FormControl
-          type="text"
-          className="form-control"
-          placeholder="Expenses detail"
-          aria-label="First name"
-        />
-      </Col>
-      <Col md="2">
-        <FormControl
-          type="number"
-          className="form-control"
-          placeholder="25.5"
-          aria-label="Amount"
-        />
-      </Col>
-      <Col md="2">
-        <FormControl
-          type="date"
-          className="form-control"
-          placeholder="Date"
-          aria-label="Date"
-        />
-      </Col>
-      <Col md="2">
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Col>
-    </Row>
+    <Form onSubmit={handleOnSubmit}>
+      <Row className="g-3 bg-dark p-4">
+        <Col md="4">
+          <FormControl
+            type="text"
+            required
+            onChange={handleOnChange}
+            className="form-control"
+            placeholder="Expenses detail"
+            aria-label="First name"
+            name="name"
+          />
+        </Col>
+        <Col md="2">
+          <FormControl
+            type="number"
+            name="amount"
+            required
+            onChange={handleOnChange}
+            className="form-control"
+            placeholder="25.5"
+            aria-label="Amount"
+          />
+        </Col>
+        <Col md="2">
+          <FormControl
+            type="date"
+            name="date"
+            required
+            onChange={handleOnChange}
+            className="form-control"
+            placeholder="Date"
+            aria-label="Date"
+          />
+        </Col>
+        <Col md="2">
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Col>
+      </Row>
+    </Form>
   );
 };
