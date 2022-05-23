2ad2938593878121d946823e0a2c9afdc1a2486b
Dipak 2056 <paudelsantosh508@gmail.com>
2022-05-23 13:54:15 +1000

    that's all for front end going to backend

--- frontend-expenses-tracker/src/components/customtable/CustomTable.jsx
+++ frontend-expenses-tracker/src/components/customtable/CustomTable.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Alert, FormCheck, ListGroup, Spinner } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Alert, Button, FormCheck, ListGroup, Spinner } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import {
   fetchExpenses,
@@ -8,6 +8,7 @@ import {
 
 export const CustomTable = () => {
   const { expenses, isLoading, res } = useSelector((state) => state.dashboard);
+  const [ids, setIds] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -19,6 +20,13 @@ export const CustomTable = () => {
       return;
     dispatch(handleOnDeleteExpenses(ids));
   };
+  const handleOnSelect = (e) => {
+    const { checked, value } = e.target;
+    checked
+      ? setIds([...ids, value])
+      : setIds(ids.filter((id) => id !== value));
+  };
+  console.log(ids);
 
   return (
     <div className="mt-5 mb-3 custom-list fs-3">
@@ -32,7 +40,12 @@ export const CustomTable = () => {
         {expenses.map((item, i) => (
           <ListGroup.Item key={i}>
             <span className="check-group">
-              <FormCheck type="checkbox" className="mr-2" />
+              <FormCheck
+                type="checkbox"
+                className="mr-2"
+                onClick={handleOnSelect}
+                value={item._id}
+              />
               <span className="title">{item.name}</span>
             </span>
 
@@ -41,6 +54,13 @@ export const CustomTable = () => {
           </ListGroup.Item>
         ))}
       </ListGroup>
+      <div className="mt-2 text-end">
+        {ids.length > 0 && (
+          <Button variant="danger" onClick={() => handleOnDelete}>
+            Delete selected Expenses
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
